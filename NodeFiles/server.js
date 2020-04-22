47cb74dfca8b3cfcb072636a38d778d77bd25fd1
Nathan Trowers <trownate@gmail.com>
2020-04-22 12:44:54 -0400

    Second Commit: Install Required Packages

---
+++ NodeFiles/server.js
@@ -0,0 +1,10 @@
+/*BEGIN***Import important modules*/
+const express = require("express");
+const server = express();
+const mongo = require("mongodb");
+/*END**Import important modules*/
+
+
+//
+/**/
+/*N*** */
