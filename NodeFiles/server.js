b95d0bd2a2e30e192d27f281f05db55fbcc9139f
Nathan Trowers <trownate@gmail.com>
2020-04-22 21:46:44 -0400

    Sixth Commit: Connect index.hmtl to Server Routing

--- NodeFiles/server.js
+++ NodeFiles/server.js
@@ -3,11 +3,15 @@ const express = require("express");
 const server = express();
 const bodyParser = require("body-parser");
 const mongo = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/"
+const time = require("./timer.js");
 /*END**Import important modules*/
 
 /*BEGIN*** Variable Declarations*/
 var uid;
 var sid = 3;
+var obj;
+var response;
 /*END*** Variable Declarations*/
 
 /*BEGIN*** Non Crud server processes*/
@@ -22,11 +26,25 @@ server.get("/", function(req, res) {
 })
 
 server.get("/process_get", function(req, res) {
-        response = {
-
-        }
+    mongo.connect(url, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        const sl = db.db("StudyLog");
+        console.log("Connected to StudyLog");
+        var user = sl.collection("Users").findOne({ name: req.query.name }, { projection: { uid: 1, name: 1 } });
+        var userS = JSON.parse(user);
+        uid = userS.uid;
+        db.close();
     })
-    /*Create Log Page*/
+    response = {
+        name: req.query.name
+    };
+    console.log(response);
+    obj = JSON.parse(response);
+    res.send(`Welcome ${obj.name}`);
+    time.waitTime(res.redirect("/session.html"));
+})
+
+/*Create Log Page*/
 
 /*View Log Pages*/
 //Query Page
