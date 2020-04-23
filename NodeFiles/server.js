8bcd9f130e1ebcffb5e5987eaf2dc581e5951cc2
Nathan Trowers <trownate@gmail.com>
2020-04-22 21:04:03 -0400

    Fifth Commit: Complete HTML Form Setup

--- NodeFiles/server.js
+++ NodeFiles/server.js
@@ -5,14 +5,28 @@ const bodyParser = require("body-parser");
 const mongo = require("mongodb").MongoClient;
 /*END**Import important modules*/
 
+/*BEGIN*** Variable Declarations*/
+var uid;
+var sid = 3;
+/*END*** Variable Declarations*/
+
 /*BEGIN*** Non Crud server processes*/
 server.use(express.static("HTMLFiles"));
 /*END*** Non Crud server processes*/
 
 /*BEGIN***routing section */
 /*Welcome/ Sign-in Page*/
+server.get("/", function(req, res) {
+    console.log("User login request received from homepage.");
+    res.sendfile(__dirname + "/" + "index.html");
+})
 
-/*Create Log Page*/
+server.get("/process_get", function(req, res) {
+        response = {
+
+        }
+    })
+    /*Create Log Page*/
 
 /*View Log Pages*/
 //Query Page
