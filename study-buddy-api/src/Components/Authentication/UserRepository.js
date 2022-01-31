64da60c1d0f5fd5b987291c12c29bda33c0a6654
NathanTrowers <trownate@gmail.com>
2022-01-30 22:47:09 -0500

    Twelfth Commit: CRUD class refactor complete
    Also added another constants file

--- study-buddy-api/src/Components/Authentication/UserRepository.js
+++ study-buddy-api/src/Components/Authentication/UserRepository.js
@@ -27,4 +27,12 @@ class UserRepository
     async deleteUser() {
 
     }
+
+/**
+ * learn OOP version of mongoDB driver implementation +++
+ * create repository classes to find the the users with
+ * figure out whether a JWT or session variable is what you want
+ * create a token generation class to generate the session token (with expiry date)
+ * unit test all of these classes before moving on.
+ */
 }
