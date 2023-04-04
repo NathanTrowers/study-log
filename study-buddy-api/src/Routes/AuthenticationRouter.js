a305b94fad63ac9129086018aa497d3f244fb173
NathanTrowers <trownate@gmail.com>
2023-04-04 08:06:34 -0400

    R6: Created route for invalidating session in SL
    Also configured intellisence for Cypress in TV

--- study-buddy-api/src/Routes/AuthenticationRouter.js
+++ study-buddy-api/src/Routes/AuthenticationRouter.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import { Router } from 'express';
-import { login, isLoggedIn} from '../Controllers/AuthenticationController.js'
+import { isLoggedIn, login, logout } from '../Controllers/AuthenticationController.js'
 const authenticationRouter = Router();
 
 authenticationRouter.get('/login', isLoggedIn);
 authenticationRouter.post('/login', login);
+authenticationRouter.post('/logout', logout);
 
 export default authenticationRouter;
