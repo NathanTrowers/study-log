563508d1aae8f900c0bbd3840567ad09754ed8e4
NathanTrowers <53019476+NathanTrowers@users.noreply.github.com>
2024-08-28 14:47:35 -0400

    STULOG-009: Added docker-compose configs for the dev, staging and prod ENVs (#26)
    I also added a volume for the mongo db files to the docker compose files, made two unit test related changes, and edited README.md.

--- tunnel-vision/src/__tests__/Utils/DataFormatter.test.js
+++ tunnel-vision/src/__tests__/Utils/DataFormatter.test.js
@@ -20,13 +20,14 @@ describe('Test Suite for the DataFormatter utility module', () => {
 
     it('tests the "formatTime" function', () => {
         /** Arrange */
-        const data = new Date(1681912549549);
+        const rawData = new Date(1681912549549);
+        const formattedData = new Date(rawData.getTime() + rawData.getTimezoneOffset()*60000);
 
         /** Act */
-        const response = formatTime(data);
+        const response = formatTime(formattedData);
 
         /** Assert */
-        expect(response).toBe('09:55:49:549');
+        expect(response).toBe('13:55:49:549');
     });
 
     it.each([
